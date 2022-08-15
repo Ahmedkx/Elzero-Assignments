ebdef0f65ea840af3b8c2ea43b4334cfeb5abb2a
Ahmedkx <midots202@gmail.com>
2022-08-15 11:26:02 +0200

    -

---
+++ TypeScript/1-11/4/main.js
@@ -0,0 +1,3 @@
+"use strict";
+let arr;
+arr = [1, 2, 3, [true], ["String", ["String", "String", 1, 123123]]];
---
+++ TypeScript/1-11/4/main.ts
@@ -0,0 +1,3 @@
+let arr: (number | boolean[] | (string | (string | number)[])[])[];
+
+arr = [1 , 2 , 3 , [true] , ["String" , ["String" , "String" , 1 ,123123]]]
